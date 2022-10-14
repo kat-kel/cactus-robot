d6dcb13a57fa085915c596750244859911f269e2
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-10-14 17:56:59 +0200

    remove unnecessary methods in Link() class

--- parse_url.py
+++ parse_url.py
@@ -53,53 +53,31 @@ class Link:
         self.subdomain = self.get_subdomain()
         self.host = ural_get_hostname(self.input)
         self.normalized_host = ural_get_normalized_hostname(self.input)
-        self.twitter_user = self.get_twitter_user()
-        self.youtube_channel_name = self.get_youtube_channel_name()
-        self.youtube_channel_id = self.get_youtube_channel_id()
-        self.facebook_group_name = self.get_facebook_group_name()
-        self.facebook_group_id = self.get_facebook_group_id()
+        self.twitter_user = None
+        self.youtube_channel_name = None
+        self.youtube_channel_id = None
+        self.facebook_group_name = None
+        self.facebook_group_id = None
+
+        if is_twitter_url(self.input):
+            screen_name = extract_screen_name_from_twitter_url(self.input)
+            if screen_name:
+                self.twitter_user = normalize_screen_name(screen_name)
+
+        if is_youtube_url(self.input):
+            parsed_url = parse_youtube_url(self.input)
+            if parsed_url and isinstance(parsed_url, YoutubeChannel):
+                self.youtube_channel_id = parsed_url.id
+                self.youtube_channel_name = parsed_url.name
+        
+        if is_facebook_url(self.input):
+            parsed_url = parse_facebook_url(self.input)
+            if parsed_url and isinstance(parsed_url, FacebookGroup):
+                self.facebook_group_id = parsed_url.id
+                self.facebook_group_name = parsed_url.handle
 
     def get_subdomain(self):
         hostname = urlparse(self.input).hostname
         subdomain = hostname.split(".")[0]
         if subdomain and subdomain != "www" and subdomain != self.domain.split(".")[0]:
             return subdomain
-
-    def get_twitter_user(self):
-        if is_twitter_url(self.input):
-            screen_name = extract_screen_name_from_twitter_url(self.input)
-            if screen_name:
-                return normalize_screen_name(screen_name)
-
-    def parse_youtube_channel(self):
-        if is_youtube_url(self.input):
-            parsed_url = parse_youtube_url(self.input)
-            if parsed_url:
-                if isinstance(parsed_url, YoutubeChannel):
-                    return parsed_url
-
-    def parse_facebook_group(self):
-        if is_facebook_url(self.input):
-            parsed_url = parse_facebook_url(self.input)
-            if parsed_url and isinstance(parsed_url, FacebookGroup):
-                return parsed_url
-    
-    def get_youtube_channel_name(self):
-        parsed_url = self.parse_youtube_channel()
-        if parsed_url:
-            return parsed_url.name
-    
-    def get_youtube_channel_id(self):
-        parsed_url = self.parse_youtube_channel()
-        if parsed_url:
-            return parsed_url.id
-
-    def get_facebook_group_name(self):
-        parsed_url = self.parse_facebook_group()
-        if parsed_url:
-            return parsed_url.handle
-    
-    def get_facebook_group_id(self):
-        parsed_url = self.parse_facebook_group()
-        if parsed_url:
-            return parsed_url.id
