700e5d319caa1dbc1b7a1691bc4ef92780364c8e
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-10-20 17:18:09 +0200

    tested xsv commands / bash script on data direct from server

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # ---------------------------------------------
 #                  COLOR
 # ---------------------------------------------
-# Color codes for console messages.
+# Color codes for console messages
 bold='\033[1m'
 reset='\033[0m'
 inverted="\033[7m"
@@ -38,7 +38,7 @@ Help()
 
 
 # ---------------------------------------------
-#                  ARGUMENTS
+#                  VARIABLES
 # ---------------------------------------------
 
 # Set variables
@@ -46,40 +46,80 @@ DATA=""
 OUTPUT=""
 LOG=""
 LENGTH="0"
-TEMPFILE="links_only.csv"
+
+# Temp files
+TEMPFILE1="data/tempfile1.csv.gz"
+TEMPFILE2="data/tempfile2.csv.gz"
+TEMPFILE3="data/tempfile3.csv.gz"
+
+# XSV commands
+SELECT_COLUMN="xsv select"
+EXPLODE_COLUMN="xsv explode"
+SEARCH_COLUMN="xsv search -s"
+
 
 # ---------------------------------------------
-#                  COUNT CSV
+#                  PREPARE DATA
 # ---------------------------------------------
+OperatingSystem()
+{
+    if [[ $(uname -s) == "Darwin" ]];
+        then
+            DECOMPRESS="gzcat"
+            COMPRESS="gzip"
+        else
+            DECOMPRESS="zcat"
+            COMPRESS="gzip"
+    fi
+}
+
 Count()
 {
     if [[ $DATA == *.gz ]]
     then
-        if [[ $(uname -s) == "Darwin" ]];
-        then
-            $(gzcat $DATA | xsv explode 'links' '|' | xsv search -s 'links' '.' | xsv select 'links' > $TEMPFILE)
-        else
-            $(zcat $DATA | xsv explode 'links' '|' | xsv search -s 'links' '.' | xsv select 'links' > $TEMPFILE)
-        fi
 
-        LENGTH=$(xsv count $TEMPFILE)
+        echo
+        echo -e "${yellow}(step 1/3) Extracting 'links' column from zipped file --> ${TEMPFILE1}${reset}"
+        $($DECOMPRESS $DATA | $SELECT_COLUMN "links" | $COMPRESS > $TEMPFILE1)
+        echo -e "${green}    Finished.${reset}"
+
+        echo
+        echo -e "${yellow}(step 2/3) Exploding into separate rows links listed together in one row --> ${TEMPFILE2}${reset}"
+        $($DECOMPRESS $TEMPFILE1 | $EXPLODE_COLUMN "links" "|" | $COMPRESS > $TEMPFILE2)
+        echo -e "${green}    Finished.${reset}"
+
+        echo
+        echo -e "${yellow}(step 3/3) Filtering out empty rows --> ${TEMPFILE3}${reset}"
+        $($DECOMPRESS $TEMPFILE2 | $SEARCH_COLUMN "links" "." | $COMPRESS > $TEMPFILE3)
+        echo -e "${green}    Finished.${reset}"
+        
+
+        LENGTH=$($DECOMPRESS $TEMPFILE3 | xsv count)
 
     elif [[ $DATA == *.csv ]]
     then
-        TEMPFILE=$DATA
-        LENGTH=$(xsv count $DATA)
+        TEMPFILE3=$DATA
+        LENGTH=$(xsv count $TEMPFILE3)
 
     fi
+}
+
+PrepareData()
+{
+    echo
+    echo "------- Preparing Gazoulloire data set for processing -------"
+    OperatingSystem
+    Count
 }
 
 # ---------------------------------------------
 #                  MAIN SCRIPT
 # ---------------------------------------------
 
+# Parse first positional argument (the data file)
 if [ $# -gt 0 ] && [ -e $1 ]
     then
         DATA=$1
-        Count
         shift
     else
         echo -e "${red}Error: A valid data file is required as the first argument positioned after run.sh${reset}"
@@ -88,6 +128,7 @@ if [ $# -gt 0 ] && [ -e $1 ]
 
 fi
 
+# Parse optional arguments (the output file, the log file)
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -109,7 +150,7 @@ do
     shift
 done
 
-
+# Prepare arguments to be passed to command line
 if [ $OUTPUT ]
 then
     OUTPUT_OPTION=" --output $OUTPUT"
@@ -124,8 +165,14 @@ else
     LOG_OPTION=""
 fi
 
+# Prepare data for processing
+PrepareData
+
+# Launch the Python script with command-line arguments
+echo
+echo "------- Processing data -------"
 echo
 echo "The command will be:"
-echo "python main.py $TEMPFILE --count ${LENGTH}${OUTPUT_OPTION}${LOG_OPTION}"
-echo
-`python main.py $TEMPFILE --count ${LENGTH}${OUTPUT_OPTION}${LOG_OPTION}`
+#`python main.py $TEMPFILE3 --count ${LENGTH}${OUTPUT_OPTION}${LOG_OPTION}`
+echo -e "${inverted}python main.py $TEMPFILE3 --count ${LENGTH}${OUTPUT_OPTION}${LOG_OPTION}${reset}"
+echo "..."
